871cac7763412091c97fead5487826e494ce6bb2
Genichiro223 <liaohx2023@shanghaitech.edu.cn>
2025-03-21 07:19:55 +0800

    modified next js

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // 添加静态导出配置
-  // output: 'export',
+  output: 'export',
   // 添加 basePath 和 assetPrefix
-  // basePath: process.env.NODE_ENV === 'production' ? '/cardiovision-lab-homepage' : '',
-  // assetPrefix: process.env.NODE_ENV === 'production' ? '/cardiovision-lab-homepage/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/cardiovision-lab-homepage' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/cardiovision-lab-homepage/' : '',
   // Enable remote images
   images: {
     // 添加 unoptimized 选项
-    // unoptimized: true,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
